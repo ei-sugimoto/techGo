311cdaab48b4ff871377a1ea2b345dcf5fed00f2
ei-sugimoto <eikun1014@gmail.com>
2024-05-19 07:21:48 +0000

    model and init add

---
+++ model/user.go
@@ -0,0 +1,18 @@
+package model
+
+type (
+	User struct {
+		ID 	 int    `json:"id"`
+		UserName string `json:"username"`
+		Password string `json:"password"`
+		Email	string `json:"email"`
+		CreatedAt string `json:"created_at"`
+	}
+
+	GetUsersRequest struct{
+	}
+	GetUsersResponce struct{
+		Users []User `json:"users"`
+	}
+)
+
