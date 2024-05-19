6a16e1b6c750e9e549b1dd30815a4ea43884351f
ei-sugimoto <eikun1014@gmail.com>
2024-05-19 06:07:26 +0000

    add test

---
+++ handler/middleware/recovery_test.go
@@ -0,0 +1,31 @@
+package middleware_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ei-sugimoto/techGO/handler/middleware"
+)
+
+
+func TestRecovery(t *testing.T) {
+	testW := httptest.NewRecorder()
+
+	testR, err := http.NewRequest("GET", "/panic", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testH := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		panic("panic test")
+	})
+
+	middleware.Recovery(testH).ServeHTTP(testW, testR)
+
+	if status := testW.Result().StatusCode; status != http.StatusInternalServerError {
+        t.Errorf("handler returned wrong status code: got %v want %v",
+            status, http.StatusInternalServerError)
+    }
+	
+}
