c1c9998906032dd94875bafd1d3c4765c49cb9da
ei-sugimoto <eikun1014@gmail.com>
2024-06-14 06:32:47 +0000

    add rarity into character

--- internal/domain/model/character.go
+++ internal/domain/model/character.go
@@ -1,10 +1,32 @@
 package model
 
+import "errors"
+
 type Character struct {
 	CharacterID string `gorm:"type:char(36);primary_key;unique;not null;"`
 	Name        string `gorm:"type:varchar(255);not null;"`
+	rarity      int    `gorm:"type:int;not null;"`
 }
 
 func (c *Character) TableName() string {
 	return "character"
+}
+
+func NewCharacter(characterID, name string, rarity int) (*Character, error) {
+	if err := isValidRarity(rarity); err != nil {
+		return nil, err
+	}
+	return &Character{
+		CharacterID: characterID,
+		Name:        name,
+		rarity:      rarity,
+	}, nil
+}
+
+func isValidRarity(rarity int) error {
+	if rarity >= 1 && rarity <= 5 {
+		return nil
+	} else {
+		return errors.New("invalid rarity")
+	}
 }
